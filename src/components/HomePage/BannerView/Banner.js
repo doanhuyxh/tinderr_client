510ed79465f8614df4561b647b98444f0ca90b77
hiep2301 <ngotranhiep2015@gmail.com>
2023-07-17 00:22:41 +0700

    call api homepage

--- src/components/HomePage/BannerView/Banner.js
+++ src/components/HomePage/BannerView/Banner.js
@@ -1,13 +1,17 @@
 import * as React from "react";
 import {Swiper, SwiperSlide} from 'swiper/react';
-
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
-
 import './Banner.scss'
-
 import {EffectCoverflow, Pagination} from 'swiper/modules';
+import anh1 from '../../../images/anh1.jpg';
+import anh2 from '../../../images/anh2.jpg';
+import anh3 from '../../../images/anh3.jpg';
+import anh4 from '../../../images/anh4.jpg';
+import anh5 from '../../../images/anh5.jpg';
+import anh6 from '../../../images/anh6.jpg';
+
 
 export default function Banner() {
     return (<>
@@ -23,31 +27,31 @@ export default function Banner() {
                 modules={[EffectCoverflow, Pagination]}
                 className="mySwiper">
                 <SwiperSlide className="w-80 banner-image__wrap">
-                    <img src="https://server.tinderr.tv/banner/20230606/ed58d2afeb4024fa429ed7afd7f2d9e1.jpg"
+                    <img src={anh4}
                          className="d-block w-100 banner-image" alt="Slide 1"></img>
                 </SwiperSlide>
                 <SwiperSlide className="w-80 banner-image__wrap">
-                    <img src="https://server.tinderr.tv/banner/20230606/eeb2bfd74f697212ae2a69e468d3122f.jpg"
+                    <img src={anh5}
                          className="d-block w-100 banner-image"
                          alt="Slide 2"></img>
                 </SwiperSlide>
                 <SwiperSlide className="w-80 banner-image__wrap">
-                    <img src="https://server.tinderr.tv/banner/20230606/a65a6b23275bdeb1613613419a0fff68.jpg"
+                    <img src={anh3}
                          className="d-block w-100 banner-image"
                          alt="Slide 3"></img>
                 </SwiperSlide>
                 <SwiperSlide className="w-80 banner-image__wrap">
-                    <img src="https://server.tinderr.tv/banner/20230606/5e2f31d2f39dc0f02ece059565e97cbd.jpg"
+                    <img src={anh1}
                          className="d-block w-100 banner-image"
                          alt="Slide 4"></img>
                 </SwiperSlide>
                 <SwiperSlide className="w-80 banner-image__wrap">
-                    <img src="https://server.tinderr.tv/banner/20230606/7fba64d5aae98aa78cdc3bc49aa88850.jpg"
+                    <img src={anh2}
                          className="d-block w-100 banner-image"
                          alt="Slide 5"></img>
                 </SwiperSlide>
                 <SwiperSlide className="w-80 banner-image__wrap">
-                    <img src="https://server.tinderr.tv/banner/20230606/f8ccbb59aee5e48bebc3ab26b6ba2760.jpg"
+                    <img src={anh6}
                          className="d-block w-100 banner-image"
                          alt="Slide 5"></img>
                 </SwiperSlide>
