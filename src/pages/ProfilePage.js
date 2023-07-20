3e9fbac5826194e16c5ac38c8a9651b449b90e41
Hiep0203 <ngotranhiep2015@gmail.com>
2023-07-20 22:32:35 +0700

    profile (demo)

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,5 +1,36 @@
 import * as React from "react";
+import "./ProfilePage.scss";
 
 export default function ProfilePage() {
-    return <h1>Cá nhân</h1>;
+    const user = {
+        name: 'Nguyễn Văn A',
+        email: 'vanA@example.com',
+        phone: '0123456789',
+    };
+
+    return (
+        <div className="profile-page">
+            <div className="profile-container">
+                <div className="profile-header">
+                    <h1>Cá nhân</h1>
+                    <button>Chỉnh sửa</button>
+                </div>
+                <div className="profile-info">
+                    <h2>Thông tin cá nhân</h2>
+                    <div className="info-item">
+                        <strong>Họ và tên: </strong> {user.name}
+                    </div>
+                    <div className="info-item">
+                        <strong>Email: </strong> {user.email}
+                    </div>
+                    <div className="info-item">
+                        <strong>Số điện thoại: </strong> {user.phone}
+                    </div>
+                </div>
+                <div className="qr-code">
+                    <p>Đây là vị trí của mã QR</p>
+                </div>
+            </div>
+        </div>
+    );
 }
